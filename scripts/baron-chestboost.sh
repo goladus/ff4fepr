1c61e8532b585134e5134c2246196253444bc799
Goladus <goladus@gmail.com>
2025-09-30 23:40:18 -0400

    scripts

---
+++ scripts/baron-chestboost.sh
@@ -0,0 +1,11 @@
+
+ff4fe-post-randomizer --edit-vchest baron-castle-rtower-1f-middle=Bacchus $roma -a
+ff4fe-post-randomizer --edit-vchest baron-castle-rtower-1f-leftstairs=Bacchus $roma -a
+ff4fe-post-randomizer --edit-vchest baron-castle-rtower-1f-left=Hermes $roma -a
+ff4fe-post-randomizer --edit-vchest baron-castle-rtower-1f-right=Hermes $roma -a
+ff4fe-post-randomizer --edit-vchest baron-castle-rtower-2f-chest-top=Lit-Bolt $roma -a
+ff4fe-post-randomizer --edit-vchest baron-castle-rtower-2f-chest-bot=Firebomb $roma -a
+ff4fe-post-randomizer --edit-vchest baron-castle-rtower-3f-pot=AgApple $roma -a
+ff4fe-post-randomizer --edit-vchest baron-castle-rtower-3f-left=Lit-Bolt $roma -a
+ff4fe-post-randomizer --edit-vchest baron-castle-rtower-3f-middle=Firebomb $roma -a
+ff4fe-post-randomizer --edit-vchest baron-castle-rtower-3f-right=Blizzard $roma -a
